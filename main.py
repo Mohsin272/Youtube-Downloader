80fd9951c6c66e61f5dbf181fd5fe1d146337365
Mohsin Tahir <mohsintahir999@gmail.com>
2021-08-21 17:48:18 +0100

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+URL = str(input("Enter video URL: "))
+my_video = YouTube(URL)
+
+print('*************** Title ***************')
+print(my_video.title)
+
+print('********** Thumbnail Image **********')
+print(my_video.thumbnail_url)
+
+print('********** Select Resolution (ITAG) **********')
+# my_video=my_video.streams.get_highest_resolution()
+for stream in my_video.streams.filter(file_extension='mp4', type='video', progressive=True):
+    print(stream)
+
+tag = int(input('ITAG: '))
+stream = my_video.streams.get_by_itag(tag)
+stream.download()
+print('Download Complete Enjoy :)')
